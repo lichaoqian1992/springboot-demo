ba1d5e5ac77db1c6360f856a3cfa30af0a574219
licq <licq@songcw.com>
2019-04-28 15:44:14 +0800

    springboot的简单demo

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,9 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * springboot的简单demo
+ */
 @SpringBootApplication
 public class DemoApplication {
 
